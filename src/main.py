0d50da2c173dd633fd17ee441e3a04584b651f04
dobidugi <dobidugi@gmail.com>
2019-02-23 04:37:34 +0900

    make parselist module

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from login import main as login
 from parselist import main as parse
+from delete import main as delete
 
+print("작동이 안될시 프로그래밍갤러리 피에로를 찾아와주세요")
 id = input("ID : ")
 pw = input("PASS : ")
-headers = login(id,pw)
-parse(headers)
+cookies, id = login(id,pw)
+print("댓글만 삭제시 1 입력후 엔터")
+print("작성글만 삭제시 2 입력후 엔터")
+print("둘다 삭제시 3 입력후 엔터")
+num = int(input())
+if(num==1):
+    cmtlist = parse(id,cookies,num)
+elif(num==2):
+    pstlist = parse(id,cookies,num)
+elif(num==3):
+    cmtlist, pstlist = parse(id,cookies,num)
--- src/parselist.py
+++ src/parselist.py
@@ -1,8 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
 
-def getlastPage(cookies):
-    
+def appendlist(tag,list):
+    for v in tag:
+        list.append(v.get("no"))
+    return list
 
-def main(cookies):
-    getlastPage(cookies)
+def getcommentlist(id,cookies):
+    _hd = {
+        "User-Agent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36",
+        "Cookie" :  cookies
+    }
+    commentlist = list()
+    page=1
+    while True:
+        url = "http://m.dcinside.com/gallog/%s?menu=R&page=%d" %(id,page)
+        res = requests.get(url,headers=_hd)
+        html = res.text
+        soup = BeautifulSoup(html,"lxml")
+        tag  = soup.find_all("a",{"class" : "del-rt"})
+        if not tag:
+            break
+        commentlist = appendlist(tag,commentlist)
+        page = page + 1
+    return commentlist
+
+def getpostlist(id,cookies):
+    _hd = {
+        "User-Agent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36",
+        "Cookie" :  cookies
+    }
+    postlist = list()
+    page=1
+    while True:
+        url = "http://m.dcinside.com/gallog/%s?menu=G&page=%d" %(id,page)
+        res = requests.get(url,headers=_hd)
+        html = res.text
+        soup = BeautifulSoup(html,"lxml")
+        tag  = soup.find_all("a",{"class" : "del-rt"})
+        if not tag:
+            break
+        postlist = appendlist(tag,postlist)
+        page = page + 1
+    return postlist
+    
+def main(id,cookies,num):
+    if(num==1):
+        commentlist = getcommentlist(id,cookies)
+        return commentlist
+    elif(num==2):
+        postlist = getpostlist(id,cookies)
+        return postlist
+    elif(num==3):
+        commentlist = getcommentlist(id,cookies)
+        postlist = getpostlist(id,cookies)
+        return commentlist, postlist
